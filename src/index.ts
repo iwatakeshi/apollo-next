73f6f285b3555ca598407baf689202886b83e55b
Takeshi <iwatakeshi@gmail.com>
2021-11-25 19:20:43 -0700

    Revert "Add core files"
    This reverts commit 54fe6cabd8722ffc2ebf8dec5b9b66873ece0880.

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./init";
-export * from "./merge";
-export * from "./use-apollo";
--- src/use-apollo.ts
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, NormalizedCacheObject } from "@apollo/client";
-import { useMemo } from "react";
-import { init } from "./init";
-import { APOLLO_STATE_PROP_NAME } from "./constants";
-
-export const useApollo = <T = NormalizedCacheObject>(client: ApolloClient<T>, props: any = {}) =>
-  useMemo(() =>
-    init(client, props[APOLLO_STATE_PROP_NAME]), [props[APOLLO_STATE_PROP_NAME]])
