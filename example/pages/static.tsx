ae37a26caf0903a547d5c4ef243857d0e068a7ab
Takeshi <iwatakeshi@gmail.com>
2023-03-04 13:24:45 -0700

    Update examples

--- example/pages/static.tsx
+++ example/pages/static.tsx
@@ -8,29 +8,35 @@ import {
 import styles from "../styles/home.module.css";
 import { createApolloClient } from "../utils/client.apollo";
 import { withApollo } from "../src";
+import Link from "next/link";
 export default function Static() {
   const { data } = useFilmsQuery();
   const films = data?.allFilms?.films;
   return (
-    <div className={styles.grid}>
-      {films?.map((film, index) => {
-        return (
-          <div
-            key={film?.episodeID}
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <h1 className={styles.title} style={{ marginBottom: "10px" }}>
-              {film.title}
-            </h1>
-            <div>Director: {film?.director}</div>
-          </div>
-        );
-      })}
+    <div className={styles.container}>
+      <Link href="/" className={styles.link}>
+        Home
+      </Link>
+      <div className={styles.grid}>
+        {films?.map((film, index) => {
+          return (
+            <div
+              key={film?.episodeID || index}
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <h1 className={styles.title} style={{ marginBottom: "10px" }}>
+                {film.title}
+              </h1>
+              <div>Director: {film?.director}</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
