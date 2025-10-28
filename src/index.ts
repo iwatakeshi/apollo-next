81b36082f3f7cf8b27069b7a6c5c35af7fe947e1
Takeshi <iwatakeshi@gmail.com>
2025-10-28 15:23:48 -0700

    refactor: clean up exports and deprecate old APIs
    - Remove init and merge from main exports (internal use only)
    - Export new types, utils, config, and usePersistentApollo
    - Deprecate merge function with JSDoc warning
    - Maintain backward compatibility

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
-export * from "./init";
-export * from "./merge";
+// Core functionality
 export * from "./use-apollo";
 export * from "./with-apollo";
+
+// Types and utilities
+export * from "./types";
+export * from "./utils";
+export * from "./config";
+export { APOLLO_STATE_PROP_NAME } from "./constants";
+
+// Optional: Persistent cache hook
+export * from "./use-persistent-apollo";
+
+// Legacy exports (deprecated - will be removed in future versions)
+/** @deprecated Use withApollo instead. Will be removed in v1.0.0 */
+export { merge } from "./merge";
