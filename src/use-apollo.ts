a5c3227086ef0879b7a938f9641077e6578d2a57
Takeshi <iwatakeshi@gmail.com>
2025-10-28 15:11:19 -0700

    fix: correct useApollo hook dependency array
    - Fix useMemo dependencies to properly track client and state changes
    - Extract state before useMemo to ensure proper memoization
    - Add comprehensive JSDoc documentation with usage examples

--- src/use-apollo.ts
+++ src/use-apollo.ts
@@ -3,9 +3,47 @@ import { useMemo } from "react";
 import { init } from "./init";
 import { APOLLO_STATE_PROP_NAME } from "./constants";
 
+/**
+ * Extracts Apollo state from Next.js page props.
+ * 
+ * @param props - Page props from Next.js
+ * @returns The Apollo state or undefined
+ */
 const getState = (props: any) => (props || {})[APOLLO_STATE_PROP_NAME];
 
+/**
+ * React hook to initialize Apollo Client with server-side state hydration.
+ * 
+ * This hook should be used in your `_app.tsx` file to create an Apollo Client
+ * that properly hydrates the cache with data from SSR/SSG.
+ * 
+ * @template T - The shape of the Apollo Client cache (defaults to NormalizedCacheObject)
+ * @param client - The Apollo Client instance to initialize
+ * @param props - Page props from Next.js containing the Apollo state
+ * @returns Memoized Apollo Client instance with hydrated state
+ * 
+ * @example
+ * ```typescript
+ * // In _app.tsx
+ * import { ApolloProvider } from '@apollo/client';
+ * import { useApollo } from '@iwatakeshi/apollo-next';
+ * import { createApolloClient } from '../lib/apolloClient';
+ * 
+ * function MyApp({ Component, pageProps }) {
+ *   const apolloClient = useApollo(createApolloClient(), pageProps);
+ *   
+ *   return (
+ *     <ApolloProvider client={apolloClient}>
+ *       <Component {...pageProps} />
+ *     </ApolloProvider>
+ *   );
+ * }
+ * ```
+ */
 export const useApollo = <T = NormalizedCacheObject>(
   client: ApolloClient<T>,
   props: unknown
-) => useMemo(() => init(client, getState(props)), [getState(props)]);
+) => {
+  const state = getState(props);
+  return useMemo(() => init(client, state), [client, state]);
+};
